7ce42809a1bdde86f9f650b209d58b319e3a444b
GonzalezKe <kg091306@gmail.com>
2022-11-28 09:54:04 -0800

    Created new scripts and added tiles
    edited RubyController script, created a new Health collectible script, added more tiles to the map, and made a new tree prefab.

--- RubysAdventureGonzalezP2/Assets/Scripts/RubyController.cs
+++ RubysAdventureGonzalezP2/Assets/Scripts/RubyController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    int currentHealth;
+
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -11,7 +17,9 @@ public class RubyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();   
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -33,5 +41,12 @@ public class RubyController : MonoBehaviour
 
         //setting the new position
         rigidbody2d.MovePosition(position);
+    }
+    
+    // Health script
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
