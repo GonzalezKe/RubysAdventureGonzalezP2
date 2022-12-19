77bd74b68663a3802414d1433f9048e807af12f2
GonzalezKe <kg091306@gmail.com>
2022-12-19 10:41:03 -0800

    Fix erros
    fixed errors

--- RubysAdventureGonzalezP2/Assets/Scripts/UIHealthBar.cs
+++ RubysAdventureGonzalezP2/Assets/Scripts/UIHealthBar.cs
@@ -6,7 +6,6 @@ public class UIHealthBar : MonoBehaviour
 {
     public static UIHealthBar instance { get; private set; }
 
-    public Image mask;
     float originalSize;
 
     void Awake()
@@ -16,12 +15,12 @@ public class UIHealthBar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        originalSize = mask.rectTransform.rect.width;   
+     
     }
 
     // Update is called once per frame
     public void SetValue(float value)
     {
-        mask.rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, originalSize * value);
+       
     }
 }
